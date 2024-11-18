15098ceb71c981f1c447c88f63ce83c11518e733
SurrealMoon <wwebsterr93@gmail.com>
2024-11-18 21:06:49 +0300

    update

--- user-frontend/src/App.jsx
+++ user-frontend/src/App.jsx
@@ -1,12 +1,21 @@
-
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import InterviewRecordingPage from './components/InterviewRecordingPage'
 
 function App() {
-  
-
+  const handleRecordingSubmit = (recordedChunks) => {
+    console.log("Recorded video chunks:", recordedChunks);
+    // Kayıt işlemi ile ilgili başka işlemler burada yapılabilir.
+  };
   return (
-    <div>
-    
-    </div>
+    <Router>
+      <Routes>
+        <Route
+          path="/video-recording/:id"
+          element={<InterviewRecordingPage onSubmit={handleRecordingSubmit} />}
+        />
+      </Routes>
+    </Router>
   )
 }
 
