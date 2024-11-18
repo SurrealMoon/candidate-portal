7033b0db6147580a564fd117809db647efad8cf8
SurrealMoon <wwebsterr93@gmail.com>
2024-11-19 00:10:45 +0300

    updttt

--- user-frontend/src/services/axios.js
+++ user-frontend/src/services/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import dotenv from "dotenv";
 dotenv.config();    
 
-const API_BASE_URL = process.env.BASE_URL;
+const API_BASE_URL = import.meta.env.VITE_BASE_URL;
 
 axios.defaults.baseURL = API_BASE_URL;
 
